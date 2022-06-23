5615d7ae3189acb419675910cecb4c8c878136a6
swapnilkrish <86659449+swapnilkrish@users.noreply.github.com>
2022-06-23 16:25:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,3 +32,21 @@ function setup() {
 function modelLoaded() {
     console.log('PoseNet Is Initialized');
 }
+
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreRightWrist = reaults[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+        leftWristX = results[0].pose.lefWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+    }
+}
